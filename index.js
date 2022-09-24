e2086c86aafeb08f59788a8e0f181f920636d36a
VU BINH CHUONG <chuongvbgch190772@fpt.edu.vn>
2022-09-24 15:13:23 +0700

    ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
+    res.write("<h2>Test nodemon</h2>");
     res.end("<h1>Heroku deployment tutorial</h1>");
 })
 .listen(process.env.PORT || 3000); 
